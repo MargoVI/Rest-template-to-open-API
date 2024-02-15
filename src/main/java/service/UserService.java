5f99a69d4a8c6fe4af373bd1641ad2804f9158d3
Margarita Ivantsova <148205607+IIMargo@users.noreply.github.com>
2024-02-15 21:13:51 +0300

    solution

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,12 @@
+package service;
+
+import model.User;
+import org.springframework.http.ResponseEntity;
+
+public interface UserService {
+    ResponseEntity<User[]> showAll();
+    String addUser(User user);
+    String updateUser(User user);
+    String deleteUser(Long id);
+
+}
